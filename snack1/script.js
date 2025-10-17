6d10c74a59890a624053decae44380dba8a70183
LucaStellato <lucastellato435@gmail.com>
2025-10-17 13:39:38 +0200

    create for cycle

--- snack1/script.js
+++ snack1/script.js
@@ -5,10 +5,16 @@ Creare un array di oggetti:
 Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 Stampare a schermo la bici con peso minore*/
 
-const RacingBikes = [
+const racingBikes = [
     {name: 'Bianchi', weight: 7},
     {name: 'Pinarello', weight: 6.8},
     {name: 'Specialized', weight: 7.2},
     {name: 'Trek', weight: 6.5},
     {name: 'Cannondale', weight: 7.1}
 ]
+
+
+
+for(let i=0; i<racingBikes.length; i++){
+   
+}
