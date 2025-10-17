c83eb112859f74a2106a5c44908063d485b8adf7
LucaStellato <lucastellato435@gmail.com>
2025-10-17 13:32:55 +0200

    first commit

---
+++ snack1/script.js
@@ -0,0 +1,14 @@
+console.log('it Work')
+
+/*Snack 1
+Creare un array di oggetti:
+Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+Stampare a schermo la bici con peso minore*/
+
+const RacingBikes = [
+    {name: 'Bianchi', weight: 7},
+    {name: 'Pinarello', weight: 6.8},
+    {name: 'Specialized', weight: 7.2},
+    {name: 'Trek', weight: 6.5},
+    {name: 'Cannondale', weight: 7.1}
+]
