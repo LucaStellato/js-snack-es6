81e07ddfe3df07f4a7588edc3c5ff1e8cdeea526
LucaStellato <lucastellato435@gmail.com>
2025-10-17 13:53:52 +0200

    create array

--- snack2/script.js
+++ snack2/script.js
@@ -7,3 +7,11 @@ Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0
 Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
 Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
 */
+
+const soccerTeams = [
+    { name: 'Juventus', points: 0, fouls: 0 },
+    { name: 'Inter', points: 0, fouls: 0 },
+    { name: 'Milan', points: 0, fouls: 0 },
+    { name: 'Napoli', points: 0, fouls: 0 },
+    { name: 'Roma', points: 0, fouls: 0 }
+]
